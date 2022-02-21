e9f607c764c6b9c4d232fb7309a47c756c997db6
Mohamad Daffa Argakoesoemah <argakoesoemahmdaffa@gmail.com>
2022-02-22 00:30:25 +0700

    add check position of point method

--- tes.py
+++ tes.py
@@ -28,6 +28,7 @@ def bubbleSortAbsis(M):
     '''
         Mengurutkan matriks M berdasarkan absis yang teurut menaik
         menggunakan algoritma bubble sort
+        Jika ada nilai absis yang sama, maka diurutkan dengan nilai ordinat yang menaik
     '''
     for i in range(len(M)-1):
         for k in range(0, len(M)-1-i):
@@ -42,6 +43,20 @@ print(type(x[0][0] < x[0][1]))
 print('\n')
 bubbleSortAbsis(x)
 print(x)
+
+def checkPointPosition(x1, y1, x2, y2, x3, y3):
+    '''
+        Mengembalikan true jika titik r(x3,y3) berada di sebelah
+        kiri atau atas garis yg dibentuk oleh p(x1,y1) dan q(x2,y2),
+        false jika tidak
+    '''
+
+    det = x1*y2 + x3*y1 + x2*y3 - x3*y2 - x2*y1 - x1*y3
+    if (det > 0):
+        return True
+    else:
+        return False
+
 
 def partisiMatrixAbsisBased(M, i, j):
     '''
