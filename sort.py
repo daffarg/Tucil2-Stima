ea688aaaa718e3ed232d0f278db78c0bd94e4ba5
Mohamad Daffa Argakoesoemah <argakoesoemahmdaffa@gmail.com>
2022-02-23 00:36:44 +0700

    add convex hull algorithm

---
+++ sort.py
@@ -0,0 +1,54 @@
+def partisiMatrixAbsisBased(M, i, j):
+    '''
+        Membagi matriks M[i][n] menjadi submatriks A[i..q][n] dan A[q+1..j][n]
+        Masukan: matriks m x n dengan setiap elemennya terdiri dari 2 elemen (absis dan oordinat)
+        Keluaran: submatriks A[i..q][n] dan A[q+1..j][n] dengan
+                  A[i..q][n] lebih kecil dari A[q+1..j][n]
+    '''
+    pivot = M[i][0] # ambil elemen pertama sbg pivot
+    pivot_y = M[i][1]
+    p = i
+    q = j + 1
+
+    count = 1
+    
+    while True:
+        while True:
+            p = p + 1
+            if (M[p][0] > pivot or (M[p][0] == pivot and M[p][1] >= pivot_y)):
+                break
+        while True:
+            q = q - 1
+            if (M[q][0] < pivot or (M[q][0] == pivot and M[q][1] <= pivot_y)):
+                break
+        M[p], M[q] = M[q], M[p]
+        if p >= q:
+            break
+    M[p], M[q] = M[q], M[p]
+    M[i], M[q] = M[q], M[i]
+
+    return q    
+
+def quickSortAbsis(M, i , j):
+    '''
+        Mengurutkan matriks M[i][n] terurut menaik menurut absisnya
+        Masukan: matriks m x n dengan setiap elemennya terdiri dari 2 elemen (absis dan oordinat)
+        Keluaran: matriks M[i][n] yang teurut menurut absisnya
+    '''
+    if (i < j):
+        k = partisiMatrixAbsisBased(M, i, j)
+        quickSortAbsis(M,i,k-1)
+        quickSortAbsis(M,k + 1,j)
+
+def bubbleSortAbsis(M):
+    '''
+        Mengurutkan matriks M berdasarkan absis yang teurut menaik
+        menggunakan algoritma bubble sort
+        Jika ada nilai absis yang sama, maka diurutkan dengan nilai ordinat yang menaik
+    '''
+    for i in range(len(M)-1):
+        for k in range(0, len(M)-1-i):
+            if (M[k+1][0] < M[k][0] or (M[k+1][0] == M[k][0] and M[k+1][1] < M[k][1])):
+                temp = M[k]
+                M[k] = M[k+1]
+                M[k+1] = temp
--- tes.py
+++ tes.py
@@ -2,11 +2,9 @@ from tkinter import Y
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import sys
-import threading
-import time
 from sklearn import datasets
 from scipy.spatial import ConvexHull
+from sort import bubbleSortAbsis, quickSortAbsis
 
 #sys.setrecursionlimit(10**7) # max depth of recursion
 #threading.stack_size(2**27)  # new thread will get stack of such size
@@ -15,131 +13,95 @@ data = datasets.load_iris()
 df = pd.DataFrame(data.data, columns=data.feature_names)
 df['Target'] = pd.DataFrame(data.target)
 # kolom target adalah nama2 bunga --> data.target_names = ['setosa' 'versicolor' 'virginica']
-print(df.shape)
-df.head()
+
+def checkPointPosition(p1, p2, p3):
+    '''
+        p1, p2, p3 adalah matriks dgn 2 elemen (elemen pertama absis, elemen kedua oordinat)
+        Mengembalikan 1 jika titik p3 berada di sebelah
+        kiri atau atas garis yg dibentuk oleh p1 dan p2,
+        -1 jika di bawahnya, 0 jika tepat pada garis
+    '''
+
+    det = p1[0]*p2[1] + p3[0]*p1[1] + p2[0]*p3[1] - p3[0]*p2[1] - p2[0]*p1[1] - p1[0]*p3[1]
+    if (det > 0):
+        return 1
+    elif (det < 0):
+        return -1
+    else:
+        return 0
+
+def distanceBetweenLineAndPoint(p1, p2, p3):
+    '''
+        p1, p2, p3 adalah matriks dgn 2 elemen (elemen pertama absis, elemen kedua oordinat)
+        Mengembalikan nilai yg sebanding dgn jarak antara titik p3
+        dgn garis yg dibentuk oleh titik p1 dan p2. Pembilang dlm
+        rumus asli perhitungan ini diabaikan sehingga yg dihitung hanya
+        penyebutnya.
+    '''
+    return abs((p2[0]-p1[0])*(p1[1]-p3[1])-(p1[0]-p3[0])*(p2[1]-p1[1]))
+
+def getLeftSide(M, p1, p2):
+    '''
+        Mendapatkan titik2 yang berada di kiri garis
+        yg dibentuk oleh p1 dan p2
+    '''
+    leftSide = []
+    for point in M:
+        if checkPointPosition(p1,p2,point) == 1:
+            leftSide.append(point)
+    return leftSide
+
+def getRightSide(M, p1, p2):
+    '''
+        Mendapatkan titik2 yang berada di kanan garis
+        yg dibentuk oleh p1 dan p2
+    '''
+    rightSide = []
+    for point in M:
+        if checkPointPosition(p1,p2,point) == -1:
+            rightSide.append(point)
+    return rightSide
+
+def convexHull(M, p1, p2, simplices, side):
+    # menemukan titik dgn jarak terjauh dari garis p1 dan p2
+    # cek semua titik untuk memastikan tidak ada titik yang sama
+    #isUnique = len(M) == len(set(M))
+    max = 0
+    max_point = [-999,-999]
+    for point in M:
+        dist = distanceBetweenLineAndPoint(p1, p2, point)
+        if (dist > max):
+            max = dist
+            max_point = point
+
+    if (max == 0): # tidak ditemukan titik lagi 
+        if (p1 not in simplices):
+            simplices.append(p1)
+        if (p2 not in simplices):
+            simplices.append(p2)
+        return
+
+    if side == 1:
+        side1 = getLeftSide(M, p1, max_point)
+        side2 = getLeftSide(M, max_point, p2)
+    else:
+        side1 = getRightSide(M, p1, max_point)
+        side2 = getRightSide(M, max_point, p2)
+
+    convexHull(side1,p1,max_point,simplices,side)
+    convexHull(side2,max_point,p2,simplices,side)
+
+def findConvexHull(M):
+    # quickSortAbsis(M, 0, len(M)-1)
+    bubbleSortAbsis(M)
+    simplices = []
+    leftSide = getLeftSide(M, M[0], M[len(M)-1])
+    rightSide = getRightSide(M, M[0], M[len(M)-1])
+    convexHull(leftSide, M[0], M[len(M)-1], simplices, 1) # sisi atas
+    convexHull(rightSide, M[0], M[len(M)-1], simplices, -1) # sisi bawah
+    print(simplices)
 
 bucket = df[df['Target'] == 0] # membagi 3 dataset iris sesuai target (0, 1, 2)
 bucket = bucket.iloc[:,[0,1]].values # mengambil atribut sepal width dan length lalu menjadikannya sbg array 2 dimensi
-# print("sebelum:")
-# print(bucket[:10])
-# print("\n")
 
-def bubbleSortAbsis(M):
-    '''
-        Mengurutkan matriks M berdasarkan absis yang teurut menaik
-        menggunakan algoritma bubble sort
-        Jika ada nilai absis yang sama, maka diurutkan dengan nilai ordinat yang menaik
-    '''
-    for i in range(len(M)-1):
-        for k in range(0, len(M)-1-i):
-            if (M[k+1][0] < M[k][0] or (M[k+1][0] == M[k][0] and M[k+1][1] < M[k][1])):
-                temp = M[k]
-                M[k] = M[k+1]
-                M[k+1] = temp
-
-# print("setelah:")
-# x = bucket[:10].tolist()
-# print(type(x[0][0] < x[0][1]))
-# print('\n')
-# bubbleSortAbsis(x)
-# print(x)
-
-def checkPointPosition(x1, y1, x2, y2, x3, y3):
-    '''
-        Mengembalikan true jika titik r(x3,y3) berada di sebelah
-        kiri atau atas garis yg dibentuk oleh p(x1,y1) dan q(x2,y2),
-        false jika tidak
-    '''
-
-    det = x1*y2 + x3*y1 + x2*y3 - x3*y2 - x2*y1 - x1*y3
-    if (det > 0):
-        return True
-    else:
-        return False
-
-
-def partisiMatrixAbsisBased(M, i, j):
-    '''
-        Membagi matriks M[i][n] menjadi submatriks A[i..q][n] dan A[q+1..j][n]
-        Masukan: matriks m x n dengan setiap elemennya terdiri dari 2 elemen (absis dan oordinat)
-        Keluaran: submatriks A[i..q][n] dan A[q+1..j][n] dengan
-                  A[i..q][n] lebih kecil dari A[q+1..j][n]
-    '''
-    pivot_index = i
-    pivot = M[i][0] # ambil elemen tengah sbg pivot
-    
-    while i < j:
-        while i < len(M) and M[i][0] <= pivot:
-            i += 1
-        # while (M[p][0] < pivot):
-        #     p += 1
-        # # M[p][0] >= pivot
-        # while (M[q][0] > pivot):
-        #     q -= 1
-        # # M[p][0] >= pivot
-        while M[j][0] > pivot:
-            j -= 1
-        if(i < j):
-            M[i], M[j] = M[j], M[i]
-        # while M[j][0] > pivot:
-        #     j -= 1
-        # if (p <= q):
-        #     # swap
-        #     temp = M[p]
-        #     M[p] = M[q]
-        #     M[q] = temp
-        #     p += 1
-        #     q -= 1
-        # else:
-        #     break
-    M[j], M[pivot_index] = M[pivot_index], M[j]
-
-    return j
-
-def partisiMatrixAbsisBased2(M, i, j):
-    '''
-        Membagi matriks M[i][n] menjadi submatriks A[i..q][n] dan A[q+1..j][n]
-        Masukan: matriks m x n dengan setiap elemennya terdiri dari 2 elemen (absis dan oordinat)
-        Keluaran: submatriks A[i..q][n] dan A[q+1..j][n] dengan
-                  A[i..q][n] lebih kecil dari A[q+1..j][n]
-    '''
-    pivot = M[i][0] # ambil elemen tengah sbg pivot
-    pivot_y = M[i][1]
-    p = i
-    q = j + 1
-
-    count = 1
-    
-    while True:
-        while True:
-            p = p + 1
-            if (M[p][0] > pivot or (M[p][0] == pivot and M[p][1] >= pivot_y)):
-                break
-        while True:
-            q = q - 1
-            if (M[q][0] < pivot or (M[q][0] == pivot and M[q][1] <= pivot_y)):
-                break
-        M[p], M[q] = M[q], M[p]
-        if p >= q:
-            break
-    M[p], M[q] = M[q], M[p]
-    M[i], M[q] = M[q], M[i]
-
-    return q    
-
-def quickSortAbsis(M, i , j):
-    '''
-        Mengurutkan matriks M[i][n] terurut menaik menurut absisnya
-        Masukan: matriks m x n dengan setiap elemennya terdiri dari 2 elemen (absis dan oordinat)
-        Keluaran: matriks M[i][n] yang teurut menurut absisnya
-    '''
-    count = 1
-    if (i < j):
-        k = partisiMatrixAbsisBased2(M, i, j)
-        quickSortAbsis(M,i,k-1)
-        quickSortAbsis(M,k + 1,j)
-
-x = bucket[:10].tolist()
-print("Sebelum: ", x)
-quickSortAbsis(x, 0, len(x)-1)
-print("Setelah: ", x)
+#findConvexHull(bucket.tolist())
